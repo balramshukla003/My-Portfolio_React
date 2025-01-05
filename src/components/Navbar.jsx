ac50afcd112f945e875b14b1c7ed3f27371938b6
Balram Shukla <balramshukla003@gmail.com>
2025-01-05 21:41:58 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,33 +3,44 @@ import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 
 const Navbar = () => {
+    const [menuActive, setMenuActive] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuActive(!menuActive);
+    };
 
     const handleScrollToTop = () => {
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+        setMenuActive(false);
     };
 
-    window.onload = () => {
-        if (window.location.hash === "#about") {
-            handleScrollToSection()
-        }
-    }
     const handleScrollToSection = (sectionId) => {
         const section = document.getElementById(sectionId);
         if (section) {
             section.scrollIntoView({ behavior: 'smooth' });
         }
+        setMenuActive(false);
     };
 
     return (
         <nav className="navbar">
             <div className="logo">Balram Shukla</div>
-            <ul className="nav-links">
+            <div className="hamburger" onClick={toggleMenu}>
+                <div className={`bar ${menuActive ? 'rotate' : ''}`}></div>
+                <div className={`bar ${menuActive ? 'fade' : ''}`}></div>
+                <div className={`bar ${menuActive ? 'rotate-reverse' : ''}`}></div>
+            </div>
+            <ul className={`nav-links ${menuActive ? 'active' : ''}`}>
                 <li><Link to="/" onClick={() => handleScrollToTop()}>Home</Link></li>
                 <li><Link to="/#about" onClick={() => handleScrollToSection('about')}>About</Link></li>
                 <li><Link to="/projects">Projects</Link></li>
                 <li><a href='https://drive.google.com/file/d/1BXWhuhYjth-3-OsaklS8I5uUuiWs6uEJ/view?usp=sharing'>Resume</a></li>
+                {/* Additional links for mobile */}
+                <li className="mobile-link"><Link to="/contact">Contact</Link></li>
+                <li className="mobile-link"><Link to="/services">Services</Link></li>
+                <li className="mobile-link"><Link to="/blog">Blog</Link></li>
             </ul>
-        </nav >
+        </nav>
     );
 };
 
